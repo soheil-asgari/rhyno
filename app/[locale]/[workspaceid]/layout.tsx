56d63d1019d27f3410afd47dd6fad11432de8ef9
soheil <soheil>
2025-08-12 15:09:43 +0330

    v3

--- app/[locale]/[workspaceid]/layout.tsx
+++ app/[locale]/[workspaceid]/layout.tsx
@@ -162,7 +162,7 @@ export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
         "gpt-4-1106-preview") as LLMID,
       prompt:
         workspace?.default_prompt ||
-        "You are a friendly, helpful AI assistant. your name is Rhyno",
+        'You are a friendly, helpful AI assistant. your name is Rhyno',
       temperature: workspace?.default_temperature || 0.5,
       contextLength: workspace?.default_context_length || 4096,
       includeProfileContext: workspace?.include_profile_context || true,
