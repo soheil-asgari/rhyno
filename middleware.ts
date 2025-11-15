adcf331146b28c1a7c489f21e4caa6ec0ca500c4
soheil_asgari <soheil_asgari@outlook.com>
2025-11-15 13:18:23 +0330

    fix company v1

--- middleware.ts
+++ middleware.ts
@@ -68,7 +68,7 @@ export async function middleware(request: NextRequest) {
   if (pathname.startsWith('/api')) {
     return response;
   }
-  const publicRoutes = ["/", '/login', '/signup', '/landing', '/chat/realtime','/company'];
+  const publicRoutes = ["/", '/login', '/signup', '/landing', '/chat/realtime','/company', '/blog', '/about', '/contact'];
   // (هر مسیر عمومی دیگری مثل /blog یا /about را به این لیست اضافه کنید)
 
   const isPublicRoute = publicRoutes.some((route: string) => {
